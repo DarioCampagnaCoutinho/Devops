91dd914adfa475ecbad944973d772d36dad1fd75
DarioCampagnaCoutinho <campagnacoutinho67@gmail.com>
2025-12-07 14:09:34 -0300

    Refactor Terraform configuration to remove deprecated AWS EC2 host resource and add AMI data source for Amazon Linux

--- 02-Terraform/Aula-02/aula02.tf
+++ 02-Terraform/Aula-02/aula02.tf
@@ -1,6 +1,4 @@
 terraform {
-  required_version = ">= 0.12"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -9,15 +7,33 @@ terraform {
   }
 }
 
-
 provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
 
-resource "aws_ec2_host" "test" {
-  instance_type     = "t3.micro"
-  availability_zone = "us-west-2a"
-  host_recovery     = "on"
-  auto_placement    = "on"
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
+
+resource "aws_instance" "this" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "ec2-terraform"
+  }
+}
+
+output "instance_id" {
+  value = aws_instance.this.id
+}
+
+output "public_ip" {
+  value = aws_instance.this.public_ip
 }
